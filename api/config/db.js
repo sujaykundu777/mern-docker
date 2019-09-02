cdf3ee74d4be8a727733ae6cc85a70500a48163a
Sujay Kundu <sujaykundu777@gmail.com>
2019-09-02 19:09:01 +0530

    dockerfile for server

--- api/config/db.js
+++ api/config/db.js
@@ -1,3 +1,4 @@
+import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 dotenv.config();
 
@@ -9,9 +10,30 @@ const {
     MONGO_DB
   } = process.env;
 
+
+const options = {
+    useNewUrlParser: true, 
+    useFindAndModify: false, 
+    useCreateIndex: true, 
+    autoIndex: false, 
+    reconnectTries: Number.MAX_VALUE,
+    reconnectInterval: 500, 
+    poolSize: 10,
+    bufferMaxEntries: 0
+  }
+
+
 const dbConnectionURL = {
-    'LOCALURL': `mongodb://${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}`
-  //  'LOCALURL': `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`
+    // 'LOCALURL': `mongodb://${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}`
+   'LOCALURL': `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`
 };
 
-export default dbConnectionURL;
+mongoose.set('debug', true);
+mongoose.connect(dbConnectionURL.LOCALURL, options);
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'Mongodb Connection Error:'));
+db.once('open', () => {
+     // we're connected !
+     console.log('Mongodb Connection Successful');
+});
+
--- api/server.js
+++ api/server.js
@@ -4,37 +4,13 @@ import path from 'path';
 import ejs from 'ejs';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
-import mongoose from 'mongoose';
-import dbConnectionURL from './config/db';
+import './config/db';
 
 // Routes
 import indexRouter from './routes';
 import userRouter from './routes/users';
 
-// mongoose options for connecting to mongodb
-const options = {
-    useNewUrlParser: true, // Use the new URL Parser
-    useFindAndModify: false,  // For findOneAndUpdate and findOneAndDelete
-    useCreateIndex: true, 
-    autoIndex: false, // Don't build Indexes
-    reconnectTries: Number.MAX_VALUE, // Never stop trying to reconnect 
-    reconnectInterval: 500, // Reconnect every 500ms
-    poolSize: 10, // Maintain up to 10 Socket Connections
-    // If not connected, return errors immediately rather than waiting for reconnect
-    bufferMaxEntries: 0
-  }
 
-  
-  // connection string 
-mongoose.set('debug', true);
-console.log(dbConnectionURL.LOCALURL);
-mongoose.connect(dbConnectionURL.LOCALURL, options);
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'Mongodb Connection Error:'));
-db.once('open', () => {
-     // we're connected !
-     console.log('Mongodb Connection Successful');
-});
 
 // Initialize app
 const app = express();
